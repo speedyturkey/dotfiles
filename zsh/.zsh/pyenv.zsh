9c2a7082824f6435291f11c2441eb68926b85d6b
Billy McMonagle <speedyturkey@gmail.com>
2021-12-20 16:22:50 -0500

    apparently ~ doesn't work

--- zsh/.zsh/pyenv.zsh
+++ zsh/.zsh/pyenv.zsh
@@ -1,5 +1,5 @@
 # initialize pyenv
 if command -v pyenv 1>/dev/null 2>&1; then
-    export PATH="~/.pyenv/shims:${PATH}"
+    export PATH="${HOME}/.pyenv/shims:${PATH}"
     eval "$(pyenv init -)"
 fi
